8f11b49d8bba20268afcd653654d555664cbc895
Dylan Starink <dylan@starink.com>
2023-09-30 14:35:04 -0700

    Fix SQL Queries

--- src/database.py
+++ src/database.py
@@ -1,5 +1,6 @@
 import os
 import dotenv
+import sqlalchemy
 from sqlalchemy import create_engine
 
 def database_connection_url():
@@ -14,7 +15,7 @@ def get_gold():
     """ """
     with engine.begin() as connection:
         result = connection.execute(
-            "SELECT gold FROM global_inventory"
+            sqlalchemy.text("SELECT gold FROM global_inventory")
         ).one()
         return result[0]
     
@@ -22,14 +23,14 @@ def add_gold(gold_to_add: int):
     """ """
     with engine.begin() as connection:
         connection.execute(
-            f"UPDATE global_inventory SET gold=gold + {gold_to_add}"
+            sqlalchemy.text(f"UPDATE global_inventory SET gold=gold + {gold_to_add}")
         )
 
 def get_red_ml():
     """ """
     with engine.begin() as connection:
         result = connection.execute(
-            "SELECT num_red_ml FROM global_inventory"
+            sqlalchemy.text("SELECT num_red_ml FROM global_inventory")
         ).one()
         return result[0]
 
@@ -37,14 +38,14 @@ def add_red_ml(red_ml_to_add: int):
     """ """
     with engine.begin() as connection:
         connection.execute(
-            f"UPDATE global_inventory SET num_red_ml=num_red_ml + {red_ml_to_add}"
+           sqlalchemy.text( f"UPDATE global_inventory SET num_red_ml=num_red_ml + {red_ml_to_add}")
         )
 
 def get_red_potions():
     """ """
     with engine.begin() as connection:
         result = connection.execute(
-            "SELECT num_red_potions FROM global_inventory"
+            sqlalchemy.text("SELECT num_red_potions FROM global_inventory")
         ).one()
         return result[0]
     
@@ -52,5 +53,5 @@ def add_red_potions(red_potions_to_add: int):
     """ """
     with engine.begin() as connection:
         connection.execute(
-            f"UPDATE global_inventory SET num_red_potions=num_red_potions + {red_potions_to_add}"
+            sqlalchemy.text(f"UPDATE global_inventory SET num_red_potions=num_red_potions + {red_potions_to_add}")
         )
