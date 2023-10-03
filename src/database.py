326d94f25ba9ea4ad1441d4f9be61418a53d9079
Dylan Starink <dylan@starink.com>
2023-10-03 09:34:13 -0700

    Add methods for arbitrary potion storage

--- src/database.py
+++ src/database.py
@@ -2,6 +2,10 @@ import os
 import dotenv
 import sqlalchemy
 from sqlalchemy import create_engine
+import typing as t
+
+from src.constants import STARTING_GOLD
+from src.models import PotionEntry
 
 def database_connection_url():
     dotenv.load_dotenv()
@@ -41,19 +45,56 @@ def add_red_ml(red_ml_to_add: int):
            sqlalchemy.text( f"UPDATE global_inventory \
                            SET num_red_ml=num_red_ml + {red_ml_to_add}")
         )
+    
+def add_potions(quantity: int, red: int, green: int, blue: int, dark: int):
+    """ """
 
-def get_red_potions():
+    if red + green + blue + dark != 100:
+        raise ValueError(f"Potion components must add up to 100 \
+                            \n(red={red}, green={green}, blue={blue}, dark={dark}) \
+                            @ quantity={quantity}")
+
+    with engine.begin() as connection:
+        
+        existing_potion_amount_or_none = connection.execute(
+            sqlalchemy.text(f"SELECT quantity \
+                                FROM potion_inventory \
+                                WHERE red={red} AND green={green} \
+                                AND blue={blue} AND dark={dark}")).first()
+
+        if existing_potion_amount_or_none is None:
+            connection.execute(
+                sqlalchemy.text(f"INSERT INTO potion_inventory \
+                                (red, green, blue, dark, quantity) \
+                                VALUES ({red}, {green}, {blue}, {dark}, {quantity})")
+            )
+        else:
+            connection.execute(
+                sqlalchemy.text(f"UPDATE potion_inventory \
+                                SET quantity=quantity + {quantity} \
+                                WHERE red={red} AND green={green} \
+                                AND blue={blue} AND dark={dark}")
+            )
+def get_potions() -> t.List[PotionEntry]:
     """ """
     with engine.begin() as connection:
         result = connection.execute(
-            sqlalchemy.text("SELECT num_red_potions FROM global_inventory")
-        ).one()
-        return result[0]
+            sqlalchemy.text("SELECT red, green, blue, dark, quantity \
+                                FROM potion_inventory")
+        ).all()
+        
+        return [PotionEntry(*row) for row in result]
     
-def add_red_potions(red_potions_to_add: int):
-    """ """
+def reset():
     with engine.begin() as connection:
         connection.execute(
-            sqlalchemy.text(f"UPDATE global_inventory \
-                            SET num_red_potions=num_red_potions + {red_potions_to_add}")
+            sqlalchemy.text(f"UPDATE global_inventory SET gold={STARTING_GOLD}")
+        )
+
+        connection.execute(
+            sqlalchemy.text("DELETE FROM carts")
+        )
+
+        connection.execute(
+            sqlalchemy.text("DELETE FROM potion_inventory")
         )
---
+++ src/models.py
@@ -0,0 +1,8 @@
+import typing as t
+
+class PotionEntry(t.NamedTuple):
+    red: int
+    green: int
+    blue: int
+    dark: int
+    quantity: int
