03157d1cd6d15d436738604eb05b37e0f81d6305
Dylan Starink <dylan@starink.com>
2023-10-05 21:37:04 -0700

    Fix weird catalog

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -17,24 +17,27 @@ def get_catalog():
     # Can return a max of 20 items.
     potions_to_sell = db.get_potions()[:20]
     
-    if len(potions_to_sell) == 0:
+    catalog = []
+    
+    for potion_entry in potions_to_sell:
+        if potion_entry.quantity == 0:
+            continue # Don't include potions we don't have
+        
+        catalog.append({
+            "sku": potion_entry.sku,
+            "name": potion_entry.sku,
+            "quantity": potion_entry.quantity,
+            "price": potion_entry.price,
+            "potion_type": potion_entry.potion_type.to_array(),
+        })
+    
+    if len(catalog) == 0:
         logger.info("No potions in inventory to list in catalog")
         return []
-    else:
-        catalog = []
-        for potion_entry in potions_to_sell:
-            if potion_entry.quantity == 0:
-                continue # Don't include potions we don't have
-            catalog.append({
-                "sku": potion_entry.sku,
-                "name": potion_entry.sku,
-                "quantity": potion_entry.quantity,
-                "price": potion_entry.price,
-                "potion_type": potion_entry.potion_type.to_array(),
-            })
-        
-        logger.info("Catalog with potions served", extra={
-            "catalog": json.dumps(catalog)
-        })
-        
-        return catalog
+    
+    
+    logger.info("Catalog with potions served", extra={
+        "catalog": json.dumps(catalog)
+    })
+    
+    return catalog
