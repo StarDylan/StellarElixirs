e277dc5556adfab7a7587e95560c944a713a9452
Dylan Starink <dylan@starink.com>
2023-10-21 21:20:13 -0700

    Add timing logic

--- src/logic/barrel_logic.py
+++ src/logic/barrel_logic.py
@@ -1,7 +1,30 @@
-from src.models import BARREL_TYPES, Barrel, BarrelStock
+from dataclasses import dataclass
+from enum import Enum
+from src.models import BARREL_TYPES, Barrel, BarrelDelta, BarrelStock
 from typing import Callable
 import copy
+import logging
 
+logger = logging.getLogger("admin")
+
+class CatalogType(Enum):
+    LARGE = 1
+    MEDIUM = 2
+    SMALL = 3
+
+
+def classify_catalog(catalog: list[Barrel]) -> CatalogType:
+    '''Classifies the catalog into one of three types: LARGE, MEDIUM, SMALL'''
+    best_value = get_best_value(BARREL_TYPES[0], catalog)
+
+    if best_value.ml_per_barrel == 10000:
+        return CatalogType.LARGE
+    elif best_value.ml_per_barrel == 2500:
+        return CatalogType.MEDIUM
+    elif best_value.ml_per_barrel == 500:
+        return CatalogType.SMALL
+    else: 
+        logger.error("Unknown Catalog Type", extra={"catalog": catalog})
 
 ### Helper functions ###
 def price(barrel: Barrel) -> int:
@@ -18,6 +41,24 @@ def type_must_be(barrel_type: list[int]) -> Callable[[Barrel], bool]:
         return barrel.potion_type == barrel_type
     
     return filter_by_color
+
+def get_best_value(barrel_type: list[int], catalog: list[Barrel]) -> Barrel:
+    '''Returns the best value barrel of the given type, given a catalog of barrels.'''
+    return max(filter(type_must_be(barrel_type),catalog), key=ml_to_gold_ratio, default=None)
+
+def determine_barrel_we_have_least_of(barrel_stock: BarrelDelta, catalog: list[Barrel]) -> list[int]:
+    '''Catalog must not be empty'''
+    
+    least_barrel_type = None
+    for barrel_type in BARREL_TYPES:
+        if len(list(filter(type_must_be(barrel_type), catalog))) == 0:
+            continue # Skip if we can't buy any of this type
+        if least_barrel_type is None:
+            least_barrel_type = barrel_type
+        elif barrel_stock.to_array()[barrel_type.index(1)] < barrel_stock.to_array()[least_barrel_type.index(1)]:
+            least_barrel_type = barrel_type
+    
+    return least_barrel_type
 
 
 def buy_one_barrel(barrel: Barrel, plan: list[Barrel], catalog: list[Barrel]):
@@ -46,18 +87,77 @@ def buy_one_barrel(barrel: Barrel, plan: list[Barrel], catalog: list[Barrel]):
     if barrel.quantity == 0:
         catalog.remove(barrel)
 
+@dataclass
+class TimelineEvent:
+    '''Includes the start/end of a timeline event, and the day of the week it occurs on.
+    INCLUSIVE, the "END" date is the last day which event will occur'''
+    day_of_week: int
+    tick: int
 
-def barrel_planner(gold: int, barrel_stock: BarrelStock, catalog: list[Barrel]) -> list[Barrel]:
+    def currently_before_ticks(self, current_dow: int, current_ticks: int, before_tick_range: int) -> bool:
+        '''Returns true if the event is within the given number of ticks.'''
+        # Must take into account the start of the event, the wrap around of day
+        # of the week and that there's 12 ticks in a day
+        '''Sat(6) is within 12 ticks of Sun()
+        '''
+
+        event_week_tick = self.day_of_week * 12 + self.tick
+
+        current_week_tick = current_dow * 12 + current_ticks
+
+        # Check for next weeks' event, since we've already passed it
+        if event_week_tick < current_week_tick:
+            event_week_tick += 7 * 12
+
+        return (event_week_tick - current_week_tick) <= before_tick_range
+
+def get_budget_ratio(day_of_week: int, tick: int, catalog: list[Barrel]) -> float:
+    '''Returns the budget ratio for the given day of the week and tick.
+    Day of week, Mon=0, Sun=6. Tick = (Hour in PST - 1) / 2'''
+
+    large_barrels_start = TimelineEvent(6, 8)
+    medium_start = TimelineEvent(5, 8)
+
+    classification = classify_catalog(catalog)
+
+    # ALWAYS buy all LARGE barrels
+    if classification == CatalogType.LARGE:
+        return 1.0
+    
+    # If we're before the large barrel event, only use 20% of our gold on any tick
+    if large_barrels_start.currently_before_ticks(day_of_week, tick, 12):
+        return 0.2
+    
+    # Always buy Medium (Only triggers after large barrels)
+    if classification == CatalogType.MEDIUM:
+        return 1.0
+
+    # If we're before the medium barrel event, only use 50% of our gold on any tick
+    if medium_start.currently_before_ticks(day_of_week, tick, 12):
+        return 0.5
+    
+    # Otherwise just use 100% of our gold
+    return 1.0
+
+
+def barrel_planner(day_of_week: int, tick: int, gold: int, barrel_stock: BarrelStock, catalog: list[Barrel]) -> list[Barrel]:
     '''Logic for buying barrels, given a budget, current barrel stock, and a catalog of barrels to buy from.
     
     Goals: Most spread of barrels possible to capture the most customers.'''
     catalog = copy.deepcopy(catalog)
-    barrel_stock = copy.copy(barrel_stock)
-    budget = gold
+    barrel_stock: BarrelDelta = barrel_stock.to_delta()
+    budget = int(gold * get_budget_ratio(day_of_week, tick, catalog))
 
     barrels_to_buy = []
 
-    
+    # Remove all barrels that aren't the best value for color
+    # Prevents us from buying MINI barrels
+    for barrel_type in BARREL_TYPES:
+        best_value_barrel = get_best_value(barrel_type, catalog)
+        
+        for barrel in filter(type_must_be(barrel_type),catalog[:]):
+            if barrel != best_value_barrel:
+                catalog.remove(barrel)
 
     while True:
         # If we buy/skip every barrel, break
@@ -68,25 +168,21 @@ def barrel_planner(gold: int, barrel_stock: BarrelStock, catalog: list[Barrel]) 
         if min(catalog, key=price).price > budget:
             break
 
-        ### OTHER CONDIITONS TO BREAK (BALKING) ###
+        # Determine barrel type that we have the least of
+        least_barrel_type = determine_barrel_we_have_least_of(barrel_stock, catalog)
 
-        for potion_type in BARREL_TYPES:
+        # Find best value barrel for potion type
+        best_value_barrel = get_best_value(least_barrel_type, catalog)
+
+        # See if we can afford it
+        if best_value_barrel.price > budget:
+            # Since we can't afford one of these, we can't afford any of them
+            catalog.remove(best_value_barrel)
+            continue
             
-            # Find best value barrel for potion type
-            best_value_barrel = max(filter(type_must_be(potion_type),catalog), key=ml_to_gold_ratio, default=None)
-
-            if best_value_barrel is None:
-                # If we can't find a barrel of this type, skip it
-                continue
-
-            # See if we can afford it
-            if best_value_barrel.price > budget:
-                # Since we can't afford one of these, we can't afford any of them
-                catalog.remove(best_value_barrel)
-                continue
-                
-            # Yay we can afford it, so let's buy one
-            buy_one_barrel(best_value_barrel, barrels_to_buy, catalog)
-            budget -= best_value_barrel.price
+        # Yay we can afford it, so let's buy one
+        buy_one_barrel(best_value_barrel, barrels_to_buy, catalog)
+        barrel_stock.add_stock(best_value_barrel.potion_type, best_value_barrel.ml_per_barrel, 1)
+        budget -= best_value_barrel.price
 
     return list(barrels_to_buy)
--- src/test/test_barrel_logic.py
+++ src/test/test_barrel_logic.py
@@ -52,6 +52,9 @@ def check_for_valid_plan(plan: list[Barrel], catalog: list[Barrel],):
             assert False, "Sku was not in catalog"
 
         total_qty_bought[barrel.sku] = barrel.quantity
+
+        if barrel.quantity <= 0:
+            assert False, "Barrel Qty was less than 0"
     
     # Check to make sure we don't buy more than available
     for catalog_barrel in catalog:
@@ -61,7 +64,7 @@ def calculate_gold_spent(plan: list[Barrel]) -> int:
     return sum(map(lambda a: a.price * a.quantity, plan))
 
 def test_barrel_logic_buys_a_barrel_when_starting_out():
-    plan = bl.barrel_planner(100, 
+    plan = bl.barrel_planner(3, 0, 100, 
                       BarrelStock(0,0,0,0), 
                       small_catalog)
     
@@ -71,7 +74,7 @@ def test_barrel_logic_buys_a_barrel_when_starting_out():
     
 
 def test_barrel_logic_buys_a_mix_when_available():
-    plan = bl.barrel_planner(320, 
+    plan = bl.barrel_planner(3, 0, 320, 
                       BarrelStock(0,0,0,0), 
                       small_catalog)
     
@@ -82,10 +85,102 @@ def test_barrel_logic_buys_a_mix_when_available():
     assert SMALL_BLUE_BARREL.sku in skus(plan)
 
 def test_barrel_logic_doesnt_buy_if_not_enough_gold():
-    plan = bl.barrel_planner(50, 
+    plan = bl.barrel_planner(5, 0, 50, 
                       BarrelStock(0,0,0,0), 
                       small_catalog)
     
     check_for_valid_plan(plan, small_catalog)
 
     assert len(plan) == 0, "Shouldn't buy anything if not enough gold"
+
+def test_never_buy_mini_barrel():
+    plan = bl.barrel_planner(3, 0, 70, 
+                            BarrelStock(0,0,0,0), 
+                            small_catalog)
+
+    check_for_valid_plan(plan, small_catalog)
+
+    assert len(plan) == 0, "Shouldn't buy mini barrel ever"
+
+def test_special_20_percent_budget():
+    plan = bl.barrel_planner(5, 9, 1250, 
+                             BarrelStock(0,1000,1000,1000),
+                             medium_catalog)
+    
+    check_for_valid_plan(plan, medium_catalog)
+
+    assert plan[0].quantity == 1, "Should buy only 1 barrel"
+    assert MEDIUM_RED_BARREL.sku == plan[0].sku, "Should buy red barrel"
+
+
+def test_special_50_percent_budget():
+    plan = bl.barrel_planner(5, 0, 400, 
+                             BarrelStock(0,600,600,600),
+                             small_catalog)
+    
+    check_for_valid_plan(plan, small_catalog)
+
+    assert plan[0].quantity == 2, "Should buy 2 barrels because 50 percent budget"
+    assert SMALL_RED_BARREL.sku == plan[0].sku, "Should buy red barrel"
+
+def test_use_all_gold_when_large_barrels():
+    plan = bl.barrel_planner(0, 0, 500, 
+                             BarrelStock(0,300,300,300),
+                             large_catalog)
+    
+    check_for_valid_plan(plan, large_catalog)
+
+    assert plan[0].quantity == 1
+    assert LARGE_RED_BARREL.sku in skus(plan)
+
+def test_use_all_gold_when_medium_barrels_past_large():
+    plan = bl.barrel_planner(0, 11, 250, 
+                             BarrelStock(0,300,300,300),
+                             medium_catalog)
+    
+    check_for_valid_plan(plan, medium_catalog)
+
+    assert plan[0].quantity == 1
+    assert MEDIUM_RED_BARREL.sku in skus(plan)
+
+
+
+
+def test_total_balance():
+    plan = bl.barrel_planner(3, 0, 300, 
+                             BarrelStock(500,0,0,0),
+                             small_catalog)
+    
+    check_for_valid_plan(plan, small_catalog)
+
+    # Should not buy any red, since we have a lot
+    assert SMALL_RED_BARREL.sku not in skus(plan)
+    assert SMALL_GREEN_BARREL.sku in skus(plan)
+    assert SMALL_BLUE_BARREL.sku in skus(plan)
+
+
+
+
+######################
+####### HELPER########
+######################
+
+def test_classify_catalog():
+    classification = bl.classify_catalog(large_catalog)
+
+    assert classification == bl.CatalogType.LARGE
+
+    classification = bl.classify_catalog(medium_catalog)
+
+    assert classification == bl.CatalogType.MEDIUM
+
+    classification = bl.classify_catalog(small_catalog)
+
+    assert classification == bl.CatalogType.SMALL
+
+def test_timeline_event():
+    assert bl.TimelineEvent(0, 5).currently_before_ticks(6, 9, 8)
+    assert not bl.TimelineEvent(0, 5).currently_before_ticks(6, 9, 7)
+
+    assert bl.TimelineEvent(5, 3).currently_before_ticks(4, 9, 6)
+    assert not bl.TimelineEvent(5, 3).currently_before_ticks(4, 9, 5)
