85807d66637a70478dc73f699d255aaa1db97e07
Dylan Starink <dylan@starink.com>
2023-11-16 15:12:01 -0800

    Don't plan any barrels

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -47,40 +47,6 @@ def post_deliver_barrels(barrels_delivered: list[Barrel]):
 def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
     """ """
 
-    with db.engine.begin() as conn:
-        db.add_historical_catalog_data(conn, wholesale_catalog)
-        
-        gold = db.get_gold(conn)
-
-        barrel_stock = db.get_barrel_stock(conn)
-        potion_stock = db.get_potions(conn)
-
-        now = datetime.now(tz=pytz.timezone("America/Los_Angeles"))
-        
-        day_of_week = now.weekday()
-        tick =int(( now.hour - 1 ) / 2)
-        
-        plan = barrel_planner(day_of_week, tick, gold, barrel_stock, wholesale_catalog, potion_stock)
-        db.add_barrel_history(conn, plan)
-
-        # Calculate total price
-        total_price = sum([barrel.price * barrel.quantity for barrel in plan])
-        total_barrels = sum([barrel.quantity for barrel in plan])
-
-
-        logger.info("Finished Barrel Planning", extra={
-            "gold": gold,
-            "gold_paid": total_price,
-            "buying_barrels": total_barrels,
-            "barrel_stock": json.dumps(barrel_stock, default=vars),
-            "catalog": json.dumps(wholesale_catalog, default=vars),
-        })
-
-        barrels_to_buy = []
-        for barrel in plan:
-            barrels_to_buy.append({
-                "sku": barrel.sku,
-                "quantity": barrel.quantity,
-            })      
+    barrels_to_buy = []
 
     return barrels_to_buy
